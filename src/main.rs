eca2cd2a462f5606c2234f492de88650cf064e91
amachang <seijro@gmail.com>
2024-09-07 23:12:02 +0900

    use jdt library

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
-use std::{fs, collections::{HashMap, HashSet}, path::{Path, PathBuf}, process::exit, io::Write};
+use std::{collections::{HashMap, HashSet}, path::PathBuf, process::exit, io::Write};
 use anyhow::Result;
-use dirs::config_dir;
 use serde::{Deserialize, Serialize};
 use clap::{crate_name, Parser};
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 #[derive(Debug, thiserror::Error)]
 enum Error {
-    #[error("Config directory not found")]
-    ConfigDirNotFound,
     #[error("Search category not found: {0}")]
     CategoryNotFound(String),
     #[error("Could not get file name for path: {0}")]
@@ -43,7 +40,9 @@ struct Opts {
 
 fn main() -> Result<()> {
     env_logger::init();
-    let config = prepare_config()?;
+    jdt::use_from(crate_name!());
+    let config = jdt::config::<Config>();
+
     log::debug!("Config: {:#?}", config);
 
     let opts = match Opts::try_parse() {
@@ -78,7 +77,7 @@ fn main() -> Result<()> {
 
     for dir in &category.dirs {
         log::debug!("Searching in dir: {}", dir);
-        let paths = walk_dir(dir);
+        let paths = jdt::walk_dir(dir, |path| path);
         log::debug!("Found {} paths", paths.len());
         for path in paths {
             let filename = path.file_name().ok_or(Error::CouldntGetFileName(path.clone()))?;
@@ -126,52 +125,5 @@ fn main() -> Result<()> {
     }
 
     Ok(())
-}
-
-fn prepare_config() -> Result<Config> {
-    let config_parent_dir = config_dir().ok_or(Error::ConfigDirNotFound)?;
-    let config_dir = config_parent_dir.join(crate_name!());
-    fs::create_dir_all(&config_dir)?;
-
-    let config_path = config_dir.join("config.toml");
-    if !config_path.exists() {
-        let default_config = Config::default();
-        let toml = toml::to_string_pretty(&default_config)?;
-        std::fs::write(&config_path, toml)?;
-        log::info!("Default config written to {:?}", config_path);
-    }
-    let config = config::Config::builder()
-        .add_source(config::File::from(config_path))
-        .build()?;
-    let config = config.try_deserialize::<Config>()?;
-
-    Ok(config)
-}
-
-fn walk_dir(dir: impl AsRef<Path>) -> Vec<PathBuf> {
-    let mut paths = Vec::new();
-    let iter = match fs::read_dir(dir.as_ref()) {
-        Ok(iter) => iter,
-        Err(err) => {
-            log::warn!("Ignoring error {} in {}", err, dir.as_ref().display());
-            return paths;
-        }
-    };
-    for entry in iter {
-        let entry = match entry {
-            Ok(entry) => entry,
-            Err(err) => {
-                log::warn!("Ignoring error {} in {}", err, dir.as_ref().display());
-                continue;
-            }
-        };
-        let path = entry.path();
-        if path.is_dir() {
-            paths.extend(walk_dir(&path));
-        } else {
-            paths.push(path);
-        }
-    }
-    paths
 }
 
